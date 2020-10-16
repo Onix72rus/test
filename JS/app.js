8fe6d656f5dc53288d643941ec71165975a5e946
Vladimir <Orgazmatron72@gmail.com>
2020-10-16 17:56:23 +0500

    EditingTheConsole ApplicationCode

--- JS/app.js
+++ JS/app.js
@@ -3,17 +3,17 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
 const personalMovieDB = {
-   count:(numberOfFilms),
-   movies:{},
-   actors:{},
-   genres:[],
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
    privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?')
-const b = prompt('На сколько оцените его?')
-const c = prompt('Один из последних просмотренных фильмов?')
-const d = prompt('На сколько оцените его?')
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
